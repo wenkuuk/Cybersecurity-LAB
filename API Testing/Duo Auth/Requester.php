d2dff1410023c2e25cac92c851fe9069add35248
3TF_9layer <3567693@gmail.com>
2022-12-20 00:14:36 -0800

    Delete API Testing/Duo Auth directory

--- API Testing/Duo Auth/Requester.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace DuoAPI;
-
-interface Requester
-{
-    public function options($options);
-    public function execute($url, $methods, $headers, $body);
-}
--- API Testing/Duo Auth/SleepService.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace DuoAPI;
-
-interface SleepService
-{
-    public function sleep($seconds);
-}
